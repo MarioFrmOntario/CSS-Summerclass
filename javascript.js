1648f641f4c939b8d076f1cf4d89482d236a052a
MarioFrmOntario <122368549+MarioFrmOntario@users.noreply.github.com>
2023-08-11 10:31:00 -0400

    Delete javascript.js

--- javascript.js
+++
@@ -1,8 +0,0 @@
-
-/*only thing this does is allow the dropdown menu on the hamburger*/
-function toggleMenu() {
-  const menu = document.querySelector(".dropdown-items");
-  const icon = document.querySelector(".menu-toggle");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
